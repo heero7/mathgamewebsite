9ce94f949b21b34c7ce1d867ec5fb208a168566e
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-21 23:21:37 -0600

    App properly displays choices and answer in one of them and displays question

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
+// variables that are initialized
 var isPlaying = false;
-const startTitle = "Start Game";
-const resetTitle = "Reset Game";
+var resetTitle = "Reset Game";
 var timeCounter = 0;
+var timeRemaining = 60;
 
+// variables linked to document elements
 var startResetBtn = document.getElementById('startreset');
 var timeDiv = document.getElementById('timeremaining');
 var time = document.getElementById('time');
 var score = document.getElementById('scorevalue');
+var question = document.getElementById('question');
+
+var choice1 = document.getElementById('box1');
+var choice2 = document.getElementById('box2');
+var choice3 = document.getElementById('box3');
+var choice4 = document.getElementById('box4');
+
+
+
+// variables not yet initialized
 var timeInterval;
 var currentTime;
-startResetBtn.onclick = function hello(){
+var questionsForGame;
+var answersForGame = [];
+startResetBtn.onclick = function beginGame(){
     if (isPlaying) {
          // reload the game
          window.location.reload();
     } else {
         // -- methods setup the game ---
-        isPlaying = !isPlaying;                 // flip the isPlaying bool
+        isPlaying = !isPlaying;                 
         startResetBtn.innerHTML = resetTitle;
         setScore();
+        questionsForGame = generateQuestions();
+        console.log(answersForGame.toString());
         startTimer();
-        // -- checks the state of the game for 60 seconds
-        //gameState();
+        displayQuestion();
     }
 }
 
 
 function startTimer() {
     timeDiv.style.display = 'block';
-    var t = 5;
+    
     timeInterval = setInterval(function() {
-        t--;
-        time.innerHTML = t;
-        currentTime = t;
-        gameState();
+        timeRemaining--;
+        time.innerHTML = timeRemaining;
+        if (timeRemaining == 0){
+            stopCountDown();
+            gameOver();
+        }
     }, 1000);
 }
 
@@ -40,10 +57,91 @@ function setScore() {
     score.innerHTML = 0;
 }
 
-function gameState() {
-    if (currentTime > 0){
-        console.log('Time: ' + currentTime)
+function stopCountDown() {
+    clearInterval(timeInterval);
+}
+
+function displayQuestion() {
+    question.innerHTML = questionsForGame.pop();
+    displayChoices();
+}
+
+function validate() {
+    
+}
+
+function displayChoices() {
+    var ansLoc = Math.floor((Math.random() * 4) + 1);
+    var b = Math.floor((Math.random() * 100) + 1);
+    var c = Math.floor((Math.random() * 100) + 1);
+    var d = Math.floor((Math.random() * 100) + 1);
+    if (ansLoc == 1) {
+        choice1.innerHTML = answersForGame.pop();
+        choice2.innerHTML = b;
+        choice3.innerHTML = c;
+        choice4.innerHTML = d;
+    } else if (ansLoc == 2) {
+        choice2.innerHTML = answersForGame.pop();
+        choice1.innerHTML = b;
+        choice3.innerHTML = c;
+        choice4.innerHTML = d;
+    } else if (ansLoc == 3) {
+        choice3.innerHTML = answersForGame.pop();
+        choice1.innerHTML = b;
+        choice2.innerHTML = c;
+        choice4.innerHTML = d;
+    } else {
+        choice4.innerHTML = answersForGame.pop();
+        choice1.innerHTML = b;
+        choice3.innerHTML = c;
+        choice2.innerHTML = d;
     }
+}
+
+function generateQuestions() {
+    var operators = ['+','x','-','/'];
+    var questions = [];
+    
+    for (var i = 0; i < 10; i++) {
+        var op = operators[Math.floor((Math.random() * operators.length))];
+        var x = Math.floor((Math.random() * 10) + 1);
+        var y = Math.floor((Math.random() * 10) + 1);
+        generateAnswer(x,y,op);
+        var equation = x.toString() + op.toString() + y.toString();
+        questions.push(equation);
+    }
+    return questions;
+}
+
+function generateAnswer(num1, num2, operator) {
+    var solution;
+    var divided = false;
+    switch(operator) {
+        case '+':
+            solution = num1 + num2;
+            break;
+        case '-':
+            solution = num1 - num2;
+            break;
+        case 'x':
+            solution = num1 * num2;
+            break;
+        case '/':
+            solution = num1 / num2;
+            divided = true;
+            break;
+    }
+    if (!divided)
+        answersForGame.push(Math.round(solution));
+    else
+        answersForGame.push(solution);
+}
+
+function gameOver() {
+    var gameOverDiv = document.getElementById('gameover');
+        gameOverDiv.style.display = 'block';
+        gameOverDiv.innerHTML = 'Game over, Score: ' + 
+            score.innerHTML +'\nPress Reset Game to try again';
 }
 // if clicked on start or reset
     // if playing
